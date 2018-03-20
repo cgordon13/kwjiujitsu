7d83ab537c61099f67c7792d7b031fbe3879e17c
CG <cgordon.13@gmail.com>
2018-03-20 17:00:19 -0400

    New Add User Activity Do the generation of the QR code in an AsyncTask Show a spinner while making the code Generate the codes up front to speed up swiping Implement delete

---
+++ app/src/main/java/com/cgordon/kwjiujitsu/AddCardActivity.java
@@ -0,0 +1,138 @@
+package com.cgordon.kwjiujitsu;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AddCardActivity extends AppCompatActivity implements TextWatcher {
+
+    private final static String TAG = AddCardActivity.class.getSimpleName();
+
+    private Button m_addButton;
+    private Button m_cancelButton;
+    private TextView m_nameText;
+    private TextView m_numberText;
+    private ProgressBar m_progressBar;
+    private TextView m_progressText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        m_nameText = findViewById(R.id.member_name);
+        m_nameText.addTextChangedListener(this);
+
+        m_numberText = findViewById(R.id.member_number);
+        m_numberText.addTextChangedListener(this);
+
+        m_addButton = findViewById(R.id.add_button);
+        m_addButton.setEnabled(false);
+        m_cancelButton = findViewById(R.id.cancel_button);
+
+        m_progressBar = findViewById(R.id.progressBar);
+        m_progressBar.setVisibility(View.INVISIBLE );
+
+        m_progressText = findViewById(R.id.progressText);
+        m_progressText.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    public void addMember(View view) {
+        Log.d(TAG, m_nameText.getText() + " " + m_numberText.getText());
+
+        m_nameText.setEnabled(false);
+        m_numberText.setEnabled(false);
+        m_cancelButton.setEnabled(false);
+        m_addButton.setEnabled(false);
+        m_progressBar.setVisibility(View.VISIBLE);
+        m_progressText.setVisibility(View.VISIBLE);
+
+        new AsyncTask<String, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(String... strings) {
+                String myStringToEncode = m_numberText.getText().toString();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
+                //https://github.com/pethoalpar/QRGenerator
+                BitMatrix bitMatrix = null;
+                try {
+                    bitMatrix = multiFormatWriter.encode(myStringToEncode, BarcodeFormat.QR_CODE, 1024, 1024);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+                FileOutputStream out = null;
+                try {
+                    String filename = getFilesDir() +"/" +   m_nameText.getText().toString();
+                    out = new FileOutputStream(filename);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+                    // PNG is a lossless format, the compression factor (100) is ignored
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (out != null) {
+                            out.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                finish();
+            }
+        }.execute(m_nameText.getText().toString(), m_numberText.getText().toString());
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (m_nameText.getText().length() > 0 && m_numberText.getText().length() > 0) {
+            m_addButton.setEnabled(true);
+        } else {
+            m_addButton.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
---
+++ app/src/main/java/com/cgordon/kwjiujitsu/Main2Activity.java
@@ -0,0 +1,305 @@
+package com.cgordon.kwjiujitsu;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main2Activity extends AppCompatActivity {
+
+    public static final String TAG = Main2Activity.class.getSimpleName();
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mSectionsPagerAdapter.refresh();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_addCard:
+
+                Intent myIntent = new Intent(this, AddCardActivity.class);
+                this.startActivityForResult(myIntent, 1);
+
+                Log.d(TAG, "Done waiting");
+
+
+//                AlertDialog.Builder data = new AlertDialog.Builder(this);
+//                data.setTitle("Add user");
+//
+////                data.setMessage("Name:");
+////                final EditText name = new EditText(this);
+////                data.setView(name);
+////
+////                data.setMessage("UID:");
+////                final EditText number = new EditText(this);
+////                number.setInputType(InputType.TYPE_CLASS_NUMBER);
+////                data.setView(number);
+//
+//
+//                LayoutInflater factory = LayoutInflater.from(this);
+//                final View addCardView= factory.inflate(R.layout.add_card, null);
+//
+//                final EditText name = addCardView.findViewById(R.id.name);
+//                final EditText number = addCardView.findViewById(R.id.number);
+//
+//                data.setView(addCardView);
+//
+//                data.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Log.d(TAG, name.getText() + " " + number.getText());
+//
+//                        String myStringToEncode = number.getText().toString();
+//                        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+//
+//                        //https://github.com/pethoalpar/QRGenerator
+//                        BitMatrix bitMatrix = null;
+//                        try {
+//                            bitMatrix = multiFormatWriter.encode(myStringToEncode, BarcodeFormat.QR_CODE, 1024, 1024);
+//                        } catch (WriterException e) {
+//                            e.printStackTrace();
+//                        }
+//                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+//                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+//
+//                        FileOutputStream out = null;
+//                        try {
+//                            String filename = getFilesDir() +"/" +   name.getText().toString();
+//                            out = new FileOutputStream(filename);
+//                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+//                            // PNG is a lossless format, the compression factor (100) is ignored
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        } finally {
+//                            try {
+//                                if (out != null) {
+//                                    out.close();
+//                                }
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//
+//                        mSectionsPagerAdapter.refresh();
+//
+//                    }
+//                });
+//
+//                data.setNegativeButton("Cancel", null);
+//
+//                data.show();
+//
+
+                return true;
+            case R.id.action_deleteCard:
+                mSectionsPagerAdapter.delete(mViewPager.getCurrentItem());
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    void printFileNames() {
+        File filesDir = getFilesDir();
+        String path = filesDir.getPath();
+
+        File[] subFiles = filesDir.listFiles();
+
+        Log.e("FILES", String.valueOf(subFiles.length));
+
+        if (subFiles != null) {
+            for (File file : subFiles) {
+                Log.e("FILE:", path + "/" + file.getAbsolutePath());
+            }
+        }
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_FILE_PATH = "file_path";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(String filename) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_FILE_PATH, filename);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
+            ImageView qrCode = rootView.findViewById(R.id.qr_image);
+
+            String filePath = getArguments().getString(ARG_FILE_PATH);
+//            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+//
+//            //https://github.com/pethoalpar/QRGenerator
+//            BitMatrix bitMatrix = null;
+//            try {
+//                bitMatrix = multiFormatWriter.encode(myStringToEncode, BarcodeFormat.QR_CODE, 1024, 1024);
+//            } catch (WriterException e) {
+//                e.printStackTrace();
+//            }
+//            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+//            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(filePath, options);
+
+            qrCode.setImageBitmap(bitmap);
+
+            File file = new File(filePath);
+            TextView name = rootView.findViewById(R.id.qr_name);
+            name.setText(file.getName());
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+
+        private File[] subFiles;
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+//            printFileNames();
+            updateFiles();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(subFiles[position].getAbsolutePath());
+        }
+
+        public void refresh() {
+            updateFiles();
+            notifyDataSetChanged();
+        }
+
+        public void delete(int index) {
+            subFiles[index].delete();
+            refresh();
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+
+        private void updateFiles() {
+            File filesDir = getFilesDir();
+            subFiles = filesDir.listFiles();
+        }
+
+        @Override
+        public int getCount() {
+            return subFiles.length;
+        }
+    }
+}
